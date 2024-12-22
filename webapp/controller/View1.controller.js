3824566d9885c05cea2edb6382270969141996a0
alanderfosse <alander.ducross@gmail.com>
2024-12-22 09:30:08 +0100

    Criando projeto

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("project1.controller.View1", {
+        onInit: function () {
+
+        },
+
+        onPress: function(){
+            alert("Testando Hello World");
+        }
+    });
+});
